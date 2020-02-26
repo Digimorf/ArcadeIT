393063aca33e942d837441bfb3b29bf158fbf873
Francesco De Simone <digimorf@users.noreply.github.com>
2020-02-26 11:00:03 +0100

    deprecated

--- ArcadeIT_Ver020_F469_CMSIS/Debug/Drivers/FATFS/source/ffsystem.d
+++
@@ -1,6 +0,0 @@
-Drivers/FATFS/source/ffsystem.o: ../Drivers/FATFS/source/ffsystem.c \
- ../Drivers/FATFS/source/ff.h ../Drivers/FATFS/source/ffconf.h
-
-../Drivers/FATFS/source/ff.h:
-
-../Drivers/FATFS/source/ffconf.h:
--- ArcadeIT_Ver020_F469_CMSIS/Release/Drivers/FATFS/source/ffunicode.d
+++
@@ -1,6 +0,0 @@
-Drivers/FATFS/source/ffunicode.o: ../Drivers/FATFS/source/ffunicode.c \
- ../Drivers/FATFS/source/ff.h ../Drivers/FATFS/source/ffconf.h
-
-../Drivers/FATFS/source/ff.h:
-
-../Drivers/FATFS/source/ffconf.h:
